46da9f4bd653c4376047969098a9ed63058a732c
matteac <mateoeestn2@gmail.com>
2022-10-15 05:23:12 -0300

    "First Commit"

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import http from "http";
+
+class Application {
+    constructor() {
+        this.routes = {};
+        this.routes_path = [];
+    }
+    get(path, callback) {
+        this.routes_path.push(path);
+        this.routes[path] = callback;
+    }
+    listen(port, callback) {
+        const server = http.createServer((req, res) => {
+            this.routes[req.url](req, res);
+        });
+        server.listen(port, callback());
+    }
+}
+
+export default Application;
